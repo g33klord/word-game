6fdb42a015e0ab201a01ea057ed13f221cd32f54
Kumar Vimal <kumar.vimal@pacewosdom.com>
2013-11-10 16:42:13 +0530

    index

--- index.php
+++ index.php
@@ -2,40 +2,48 @@
 <html>
 	<head>
 	<script src="js/jquery-1.9.1.js"></script>
+	<!--<script src="js/jquery-ui-1.10.3.custom.js"></script> -->
+	<script type='text/javascript' > 
+	window.points = 0; 
+	window.QuesCount =0;
+	window.AnsCount =0;
+	</script>
      <script src='script1.js'></script>
 	<title>The Game</title>
 	
 	</head>
+
 	<body>
 		<script language="javascript">
-			var max_time = 10;  //Countdown Time Limit
+			var max_time = 20;  //Countdown Time 
 			var cinterval;
-			 //var points = 0;  //For score
+			 var points = 0;
 			function countdown_timer(){
-			  max_time--;  //count down
+			  max_time--;
 			  document.getElementById('countdown').innerHTML = max_time;
-			  if(max_time == 0){   //if time = o sec, clear interval.
+			  if(max_time == 0) {  //if time = o sec, clear interval.
 			    clearInterval(cinterval);
-			    alert('Game Over!');   //you lose
-			    document.location = 'home.html'; //redirect to home
+			    alert('Game Over!');
+			     alert('Points:' + window.points + '| Ques Count:' + window.QuesCount);
+			    document.location = 'home.html';
 			  }
 			}
-			cinterval = setInterval('countdown_timer()', 1000); //1000 = 1 sec
+			cinterval = setInterval('countdown_timer()', 1000);
 		</script> 
 		<div id="content">
-		Seconds:<div id="countdown">10</div>
-		Score: <div id="score"><div>
+		Seconds:<div id="countdown">20</div>
+		Score: <div id="score">0<div>
 		
 		<?php include_once("con.php");  //connect to database
-			$result = mysqli_query($con,"SELECT count(*) FROM questions"); //get no of rows
+			$result = mysqli_query($con,"SELECT count(*) FROM questions"); 
 			$re=mysqli_fetch_row($result); //No of rows = re
 			$random=rand(1,$re[0]);	 //random number between 1 and re.
 		 ?>
 		<script>
 			var qid=<?php echo $random; ?>;
-			var request = $.ajax({ //ajax request
-			url: "question.php",  
-			type: "POST",  //pass id to qistion.php
+			var request = $.ajax({
+			url: "question.php",
+			type: "POST",
 			data: { id : qid },
 			dataType: "html"
 			});
@@ -47,7 +55,6 @@
 		<div id="question"></div>
 		<div class="wordBox">
 			<input type="text"/>
-			<div class="question"></div>
 			<button>skip</button>
 			<hr>
 			<div class="definitionBox">	</div>
